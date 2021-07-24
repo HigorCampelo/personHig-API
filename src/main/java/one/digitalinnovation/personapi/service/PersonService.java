0e57a93a9fc68b2e7b8f08325b61e264bcee6564
Higor Campelo <higorxadrez@hotmail.com>
2021-07-24 12:01:51 -0300

    Create Person

---
+++ src/main/java/one/digitalinnovation/personapi/service/PersonService.java
@@ -0,0 +1,27 @@
+package one.digitalinnovation.personapi.service;
+
+import one.digitalinnovation.personapi.dto.MessageResponseDTO;
+import one.digitalinnovation.personapi.entity.Person;
+import one.digitalinnovation.personapi.repository.PersonRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
+
+
+@Service
+public class PersonService {
+
+    private PersonRepository personRepository;
+
+    public PersonService (PersonRepository personRepository){
+        this.personRepository = personRepository;
+    }
+
+    @PostMapping
+    public MessageResponseDTO createPerson(Person person){
+        Person savePerson = personRepository.save(person);
+        return MessageResponseDTO
+                .builder()
+                .message("Created Person with ID " + savePerson.getId())
+                .build();
+    }
+}
